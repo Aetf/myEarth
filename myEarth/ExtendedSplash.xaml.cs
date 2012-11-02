d2adad0adb932837a2cf92d4677d8a923f3e8564
Aetf <7437103@gmail.com>
2012-11-02 23:33:43 +0800

    改成MSDN里建议的方式实现SplashScreen的延时

---
+++ myEarth/ExtendedSplash.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace myEarth
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    sealed partial class ExtendedSplash
+    {
+        internal Rect splashImageRect;
+        internal bool dismissed = false;
+        internal Frame rootFrame;
+
+        private SplashScreen splash;
+        public ExtendedSplash(SplashScreen splashscreen, bool loadState)
+        {
+            this.InitializeComponent();
+
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+
+            splash = splashscreen;
+
+            if(splash != null)
+            {
+                splash.Dismissed += new TypedEventHandler<SplashScreen, object>(DismissedEventHandler);
+
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+            }
+
+            rootFrame = new Frame();
+
+            RestoreStateAsync(loadState);
+        }
+
+        async void RestoreStateAsync(bool loadState)
+        {
+            if (loadState)
+                await SuspensionManager.RestoreAsync();
+
+            // Normally you should start the time consuming task asynchronously here and 
+            // dismiss the extended splash screen in the completed handler of that task
+            // This sample dismisses extended splash screen  in the handler for "Learn More" button for demonstration
+        }
+
+        // Position the extended splash screen image in the same location as the system splash screen image.
+        void PositionImage()
+        {
+            extendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            extendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            extendedSplashImage.Height = splashImageRect.Height;
+            extendedSplashImage.Width = splashImageRect.Width;
+        }
+
+        void ExtendedSplash_OnResize(Object sender, WindowSizeChangedEventArgs e)
+        {
+            // Safely update the extended splash screen image coordinates. This function will be fired in response to snapping, unsnapping, rotation, etc...
+            if (splash != null)
+            {
+                // Update the coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+            }
+        }
+
+        // Include code to be executed when the system has transitioned from the splash screen to the extended splash screen (application's first view).
+        void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+
+            // Navigate away from the app's extended splash screen after completing setup operations here...
+            this.rootFrame.Navigate(typeof(SelectBatteryType));
+        }
+    }
+}
